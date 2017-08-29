88c2ba8c033bdb52acb7aa976fe9f6d4c2f0373a
Philipp Weitz <philipp.weitz@futurice.com>
2017-08-29 23:36:56 +0300

    added ios support (basic structure)

---
+++ ios/ReactNativeAndroidLocationSwitch.m
@@ -0,0 +1,32 @@
+
+#import "ReactNativeAndroidLocationSwitch.h"
+#import "UIKit/UIKit.h"
+#import <CoreLocation/CoreLocation.h>
+
+@implementation ReactNativeAndroidLocationSwitch
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+
+RCT_REMAP_METHOD(enableLocationService,
+                 onPermissionGiven:(RCTResponseSenderBlock)successCallback
+                 onPermissionDenied:(RCTResponseSenderBlock)errorCallback)
+{
+    if ([CLLocationManager locationServicesEnabled]) {
+        NSLog(@"Location Services Enabled");
+        successCallback(@[[NSNull null]]);
+        
+    } else {
+        NSLog(@"Location Services Disabled");
+        // show location settings
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+        errorCallback(@[[NSNull null]]);
+    }
+}
+
+@end
+
