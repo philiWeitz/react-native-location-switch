0f78304db809e720fde4dc094c138c4ead8771cb
Miyaguisan <octavio.miyagui@gmail.com>
2019-09-10 02:52:54 -0500

    RN 0.6.5 Update (#1)
    * Added REQUIRED home description to podspec. Updated RNReactNativeLocationSwitch.m to avoid App STore reject. Bump package version.
    
    * Included information from package file. Renamed podspec file
    
    * Updataed target version

--- ios/RNReactNativeLocationSwitch.m
+++ ios/RNReactNativeLocationSwitch.m
@@ -1,37 +1,37 @@
-
 #import "RNReactNativeLocationSwitch.h"
 #import "UIKit/UIKit.h"
 #import <Foundation/NSURL.h>
 #import <CoreLocation/CoreLocation.h>
 
+
 @implementation RNReactNativeLocationSwitch
 
-- (dispatch_queue_t)methodQueue
-{
+- (dispatch_queue_t)methodQueue {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
 
+RCT_EXPORT_MODULE()
 
 RCT_REMAP_METHOD(enableLocationService,
                  onPermissionGiven:(RCTResponseSenderBlock)successCallback
-                 onPermissionDenied:(RCTResponseSenderBlock)errorCallback)
-{
+                 onPermissionDenied:(RCTResponseSenderBlock)errorCallback) {
     CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
-
-    if (![CLLocationManager locationServicesEnabled]) {
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"App-Prefs:root=Privacy&path=LOCATION"] options:@{}
-                                 completionHandler:^(BOOL success) {}];
-
-    } else if (status == kCLAuthorizationStatusDenied) {
-        NSLog(@"Location Services Disabled");
+    
+    if (![CLLocationManager locationServicesEnabled] || status == kCLAuthorizationStatusDenied) {
+        NSURL *serviceURL = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
         
-        // show location settings
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{}
-                                 completionHandler:^(BOOL success) {}];
-
-    } else {
-        NSLog(@"Location Services Enabled");
+        if ([[UIApplication sharedApplication] canOpenURL:serviceURL]) {
+            if (@available(iOS 10.0, *)) {
+                [[UIApplication sharedApplication] openURL:serviceURL options:@{} completionHandler:^(BOOL success) {}];
+            } else {
+                [[UIApplication sharedApplication] openURL:serviceURL];
+            }
+        }
+        else {
+            errorCallback(@[[NSNull null]]);
+        }
+    }
+    else {
         successCallback(@[[NSNull null]]);
     }
 }
@@ -39,15 +39,12 @@ RCT_REMAP_METHOD(enableLocationService,
 
 RCT_REMAP_METHOD(isLocationEnabled,
                  onLocationEnabled:(RCTResponseSenderBlock)successCallback
-                 onLocationDisable:(RCTResponseSenderBlock)errorCallback)
-{
+                 onLocationDisable:(RCTResponseSenderBlock)errorCallback) {
     CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
     
     if (![CLLocationManager locationServicesEnabled] || status == kCLAuthorizationStatusDenied) {
-        NSLog(@"Location Services Disabled");
         errorCallback(@[[NSNull null]]);
     } else {
-        NSLog(@"Location Services Enabled");
         successCallback(@[[NSNull null]]);
     }
 }
---
+++ ios/react-native-location-switch.podspec
@@ -0,0 +1,22 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, '../package.json')))
+
+
+Pod::Spec.new do |s|
+  s.authors      = package['author']
+  s.homepage     = package['homepage']
+  s.license      = package['license']
+  s.name         = package['name']
+  s.summary      = package['description']
+  s.version      = package['version']
+
+  s.platform     = :ios, "8.0"
+  s.requires_arc = true
+  s.source       = { :git => "https://github.com/philiWeitz/react-native-android-location-switch.git", :tag => "master" }
+  s.source_files = "*.{h,m}"
+
+  s.dependency 'React'
+
+
+end
